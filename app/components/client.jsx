a06737fae5d7b59519f7293b84295fc4403c5801
Ziaul Sarker <ziaulsarker@gmail.com>
2018-04-06 18:01:27 -0400

    js

--- app/components/client.jsx
+++ app/components/client.jsx
@@ -8,9 +8,9 @@ const Client = (props) => {
                 <div className="ContactCard-headings">
                     <h3> Company Name : <span>  { props.companyName.toUpperCase() } </span> </h3>
                     <h3> Contact Name : <span> { props.contactName.toUpperCase() } </span></h3>
-                    <h3> Office Phone : <span> { props.officePhone.toUpperCase() } </span> </h3>
-                    <h3> Cell Phone : <span> { props.cellPhone.toUpperCase() } </span> </h3>
-                    <h3> Email Address : <span> { props.email.toUpperCase() } </span> </h3>
+                    <h3> Office Phone : <a href={`tel:${props.officePhone.toLowerCase()}`}> <span> { props.officePhone.toUpperCase() } </span> </a></h3>
+                    <h3> Cell Phone : <a href={`tel:${props.cellPhone.toLowerCase()}`}> <span> { props.cellPhone.toUpperCase() } </span> </a></h3>
+                    <h3> Email Address : <a href={`mailto:${props.email.toLowerCase()}`}> <span> { props.email.toUpperCase() } </span> </a></h3>
                     <h3> Fax Number : <span> { props.fax.toUpperCase() } </span> </h3>
                     <h3> Company Location : { props.location.toUpperCase() } </h3>
                 </div>
